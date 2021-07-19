973661b529d06a315f9c92b141d5a24fc317efd8
colyud <33720136+colyud@users.noreply.github.com>
2021-07-19 17:04:30 -0500

    add actions and footer to post component

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,9 +1,16 @@
+import { useRef } from "react";
 import Image from "./image";
+import Footer from "./footer";
+import Actions from "./actions";
 
 export default function Post({ content }) {
+    const commentInput = useRef(null);
+    const handleFocus = () => commentInput.current.focus();
     return (
         <div className="rounded col-span-4 border bg-white mb-16">
             <Image src={content.imageSrc} caption={content.caption} />
+            <Actions docId={content.docId} totalLikes={content.likes.length} likedPhoto={content.userLikedPhoto} handleFocus={handleFocus} />
+            <Footer caption={content.caption} username={content.username} />
         </div>
     );
 }

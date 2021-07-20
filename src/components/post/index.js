290a381f7c87cb236606d368bf6ad0e24cb61315
colyud <33720136+colyud@users.noreply.github.com>
2021-07-20 12:16:09 -0500

    add comment functional to post component

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import Image from "./image";
 import Footer from "./footer";
 import Actions from "./actions";
+import Comments from "./comments";
 
 export default function Post({ content }) {
     const commentInput = useRef(null);
@@ -11,6 +12,7 @@ export default function Post({ content }) {
             <Image src={content.imageSrc} caption={content.caption} />
             <Actions docId={content.docId} totalLikes={content.likes.length} likedPhoto={content.userLikedPhoto} handleFocus={handleFocus} />
             <Footer caption={content.caption} username={content.username} />
+            <Comments docId={content.docId} comments={content.comments} posted={content.dateCreated} commentInput={commentInput} />
         </div>
     );
 }

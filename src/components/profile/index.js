81051b8fff8b68c5117010326e95624b14d07487
colyud <33720136+colyud@users.noreply.github.com>
2021-08-17 15:55:12 -0500

    useReducer and get all photos by username

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,12 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import Header from "./header";
 import Photos from "./photos";
+import { getUserByUsername, getUserPhotosByUsername } from "../../services/firebase";
+
+const reducer = (state, newState) => ({ ...state, ...newState });
+const initialState = {
+    profile: {},
+    photosCollection: [],
+    followerCount: 0,
+};
 
 export default function Profile({ username }) {
+    const [{ profile, photosCollection, followerCount }, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        // think about caching here in localstorage so we don't make a network call we have already made previously (task)
+        async function getProfileInfoAndPhotos() {
+            const [{ ...user }] = await getUserByUsername(username);
+            const photos = await getUserPhotosByUsername(username);
+            console.log(photos);
+            dispatch({ profile: user, photosCollection: photos, followerCount: user.followers.length });
+        }
+        getProfileInfoAndPhotos();
+    }, [username]);
+
     return (
         <>
-            <Header />
-            <Photos />
+            <Header photosCollection={photosCollection.length} profile={profile} followerCount={followerCount} setFollowerCount={dispatch} />
+            <Photos photos={photosCollection} />
         </>
     );
 }
